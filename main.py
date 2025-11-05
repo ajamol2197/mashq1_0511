6bdce1757d525de85dc1c05b9854c0983ba59b06
ajamol2197 <ajamol2197@gmail.com>
2025-11-05 18:03:13 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+# 1-m
+for a in range(50):
+    print(a)
+    if a == 42:
+        break
+
+# 2-m
+for a in range(21):
+    if a % 2 == 0:
+        print(a)
+
+# 3-m
+for a in range(31):
+    print(a)
+    if a == 5:
+        break
+
+# 4-m
+for a in range(15):
+    if a % 3 != 0:
+        print(a)
+
+# 5-m
+for a in range(21):
+    if a == 25:
+        break
